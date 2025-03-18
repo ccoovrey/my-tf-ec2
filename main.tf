64fc080672b2db1c8bb0d7c4e63867d8eda34672
ccoovrey <coovrey@gmail.com>
2025-03-18 12:20:24 -0500

    updates to dev

--- main.tf
+++ main.tf
@@ -14,7 +14,6 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami = "ami-08b5b3a93ed654d19"      # ami is located in us-east-1 
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.webtraffic.name]
 }
 
 # we are using my default vpc - so don't need allow https basic
