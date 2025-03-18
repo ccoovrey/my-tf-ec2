84aba6eabbdc08df126212766192485fd1201629
ccoovrey <coovrey@gmail.com>
2025-03-18 12:44:12 -0500

    updates to dev

--- main.tf
+++ main.tf
@@ -14,16 +14,4 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami = "ami-08b5b3a93ed654d19"      # ami is located in us-east-1 
   instance_type = "t2.micro"
-  subnet_id = "subnet-005f51eeefd041240" # subnet in my default vpc
-}
-
-# we are using my default vpc - so don't need allow https basic
-# also no need for allow https dynamic
-
-resource "aws_eip" "elasticeip" {
-  instance = aws_instance.ec2.id
-}
-
-output "EIP" {
-    value = aws_eip.elasticeip.public_ip
 }
