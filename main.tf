282746c77690606a340bd196c8c3f4492d03ce09
ccoovrey <coovrey@gmail.com>
2025-03-18 12:18:42 -0500

    updates to dev

--- main.tf
+++ main.tf
@@ -17,59 +17,8 @@ resource "aws_instance" "ec2" {
   security_groups = [aws_security_group.webtraffic.name]
 }
 
-# simple security group
-resource "aws_security_group" "webtraffic" {
-    name = "Allow HTTPS Basic"
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-    egress {
-        from_port = 443
-        to_port = 443
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-# dynamic blocks security group
-variable "ingressrules" {
-    type = list(number)
-    default = [80,443]
-}
-
-variable "egressrules" {
-    type = list(number)
-    default = [80,443,25,3306,53,8080]
-}
-
-resource "aws_security_group" "webtraffic2" {
-    name = "Allow HTTPS Dynamic"
-    dynamic "ingress" {
-        iterator = port
-        for_each = var.ingressrules
-        content {
-            from_port = port.value
-            to_port = port.value
-            protocol = "TCP"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
-    }
-    
-    dynamic "egress" {
-        iterator = port
-        for_each = var.egressrules
-        content {
-            from_port = port.value
-            to_port = port.value
-            protocol = "TCP"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
-    }
-}
+# we are using my default vpc - so don't need allow https basic
+# also no need for allow https dynamic
 
 resource "aws_eip" "elasticeip" {
   instance = aws_instance.ec2.id
